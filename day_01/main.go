c4a71f672caa62841b8c43d63d00ed2c1bc144a9
Auryn Engel <auryn_engel@mckinsey.com>
2023-12-01 14:40:57 +0100

    finish day 1

--- day_01/main.go
+++ day_01/main.go
@@ -3,6 +3,9 @@ package aoc
 import (
 	"fmt"
 	"os"
+  "strings"
+  "strconv"
+  "errors"
 )
 
 func Check(e error) {
@@ -21,6 +24,101 @@ func main() {
 
 }
 
-func DayOne(input string) (string) {
-  return "Hello"
+func DayOne(input string) (int) {
+  lines := strings.Split(input, "\n")
+  sum := 0
+  for _, line := range lines {
+    if len(line) == 0 {
+      continue
+    }
+    chars := strings.Split(line, "")
+    numbers := make([]int, 0)
+    for _, char := range chars {
+      num, err := strconv.Atoi(char)
+      if err == nil {
+        numbers = append(numbers, num)
+      }
+  }
+    first := numbers[0]
+    last := numbers[len(numbers)-1]
+    sum += 10*first + last
+  }
+
+  return sum
+
+}
+
+func getNumFromString(subString string) (int, error) {
+  if strings.HasPrefix(subString,"one"){
+
+    return 1, nil
+  }
+    if strings.HasPrefix(subString,"two"){
+
+    return 2, nil
+  }
+
+  if strings.HasPrefix(subString,"three"){
+
+    return 3, nil
+  }
+  if strings.HasPrefix(subString,"four"){
+
+    return 4, nil
+  }
+  if strings.HasPrefix(subString,"five"){
+
+    return 5, nil
+  }
+  if strings.HasPrefix(subString,"six"){
+
+    return 6, nil
+  }
+  if strings.HasPrefix(subString,"seven"){
+
+    return 7, nil
+  }
+  if strings.HasPrefix(subString,"eight"){
+
+    return 8,nil
+  }
+
+  if strings.HasPrefix(subString,"nine"){
+
+    return 9,nil
+  }
+
+  return 0, errors.New("No number found")
+
+}
+
+func DayTwo(input string) (int) {
+  lines := strings.Split(input, "\n")
+  sum := 0
+  for _, line := range lines {
+    if len(line) == 0 {
+      continue
+    }
+    chars := strings.Split(line, "")
+    numbers := make([]int, 0)
+    for i, char := range chars {
+      num, err := strconv.Atoi(char)
+      if err == nil {
+        numbers = append(numbers, num)
+      }
+      substring := line[i:]
+      num, err = getNumFromString(substring)
+      if err == nil {
+      numbers = append(numbers, num)
+            }
+
+      
+  }
+    first := numbers[0]
+    last := numbers[len(numbers)-1]
+    sum += 10*first + last
+  }
+
+  return sum
+
 }
--- day_01/main_test.go
+++ day_01/main_test.go
@@ -6,16 +6,24 @@ import (
   "os"
 )
 
-func TestAdd(t *testing.T){
-
+func TestDayOne(t *testing.T){
 	dat, err := os.ReadFile("input.txt")
   Check(err)
 
   got := DayOne(string(dat))
-  want := "Hello"
-
-
+  want := 54708
     if got != want {
-        t.Errorf("got %q, wanted %q", got, want)
+        t.Errorf("got %d, wanted %d", got, want)
+    }
+}
+
+func TestDayTwo(t *testing.T){
+	dat, err := os.ReadFile("input.txt")
+  Check(err)
+
+  got := DayTwo(string(dat))
+  want := 54087
+    if got != want {
+        t.Errorf("got %d, wanted %d", got, want)
     }
 }
